76b6c65023ef978a2d68171be954abdc94aab61b
alejandrotoro18 <alejandrotoro18@gmail.com>
2022-08-15 22:53:06 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-fmy5eq
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-fmy5eq)
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+@NgModule({
+  imports: [
+    CommonModule
+    
+  ],
+  declarations: [SidebarComponent]
+  export:
+  [ SidebarComponent]
+})
+export class SharedModule { }
